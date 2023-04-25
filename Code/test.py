1bd18f8c6e4eabbb2c5252c22e61a8f217983e5e
Chunt0 <chunt.mail@gmail.com>
2023-04-25 10:59:54 -0700

    Updated

--- Code/test.py
+++ Code/test.py
@@ -1,14 +1,15 @@
-import numpy as np
+import clifford as cf
+from clifford.tools.g3 import angle_between_vectors, euc_mv_to_np
 
-# Define a vector
-v = np.array([1, 0])
+# Create a 3D geometric algebra space
+layout, blades = cf.Cl(3)
 
-# Define the angle of rotation in radians
-theta = np.pi/4
+# Define two conformal vectors
+v1 = 1 + 2*blades['e1'] + 3*blades['e2'] + 4*blades['e3']
+v2 = 2 + 3*blades['e1'] + 1*blades['e2'] - 2*blades['e3']
 
-# Compute the rotated vector using Euler's formula
-rotated_v = np.exp(1j*theta) * v
+# Compute the angle between the vectors
+v1_np = euc_mv_to_np(v1)
+v2_np = euc_mv_to_np(v2)
 
-# Print the original and rotated vectors
-print(f'Original vector: {v}')
-print(f'Rotated vector: {rotated_v}')
+print(v1_np)
--- Code/vector_rotation_ga.py
+++ Code/vector_rotation_ga.py
@@ -1,51 +1,65 @@
 import clifford as cf
+from clifford.tools.g3 import euc_mv_to_np
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Creat a 2D geometric algebra space
-layout, blades = cf.C1(2)
+# Create a 2D geometric algebra space
+layout, blades = cf.Cl(3)
 
 # Define the basis vectors
 e1 = blades['e1']
 e2 = blades['e2']
+e3 = blades['e3']
 e12 = blades['e12']
 
-# My original 2d vector
+# Define my original 2d vector
 x = e1
 
-# My two vectors that I will use to rotate x by the angle between
+# Define the two vectors that I will use to rotate x by the angle between
 # Current angle = pi/4
-v1 = 1*e1 # [1,0]
-v2 = 1*e1 + 1*e2 # [1,1]
+v1 = 1*e1 + 0*e2 + 0*e3 # [1,0,0]
+v2 = 1*e1 + 1*e2 + 0*e3 # [1,1,0]
+
+# Find the angle between these two vectors
+# Gotta convert the vector to a np.array, the function angle_between_vectors
+# Doesn't seem to work...
+v1_np = euc_mv_to_np(v1)
+v2_np = euc_mv_to_np(v2)
+cos_theta = np.dot(v1_np, v2_np) / (np.linalg.norm(v1_np) * np.linalg.norm(v2_np))
+theta = np.arccos(cos_theta)
 
 # Rotate my vector x
 x_rot = v1*v2*x
-print(x_rot)
-if np.linalg.norm(x_rot[0]) == 0: 
-    x_rot = np.array([np.real(x_rot[1]), np.imag(x_rot[1])])
-else:
-    x_rot = np.array([np.real(x_rot[0]), np.imag(x_rot[0])])
-print(x_rot)
+print(f"Rotation Angle: {theta*(180/np.pi)}")
+print(f"Original Vector: {x}")
+print(f"Rotated vector: {x_rot}")
 
-# Show the plot
-plt.show()# Extract the x and y components of the vectors
-x = [x(e1), x_rot(e1)]
-y = [x(e2), x_rot(e2)]
+# Convert x and x_rot into np.array in order to plot
+x_np = euc_mv_to_np(x)
+x_rot_np = euc_mv_to_np(x_rot)
 
 # Create a figure and axis object
 fig, ax = plt.subplots()
 
 # Plot the vectors
-ax.quiver([0, 0], [0, 0], x, y, angles='xy', scale_units='xy', scale=1, color=['r', 'b'])
+ax.quiver(0, 0, x_np[0], x_np[1], angles='xy', scale_units='xy', scale=1, color='r')
+ax.quiver(0, 0, x_rot_np[0], x_rot_np[1], angles='xy', scale_units='xy', scale=1, color='b')
+
+# Calculate the angle between the original vector and the rotated one
+cos_theta = np.dot(x_np, x_rot_np) / (np.linalg.norm(x_np) * np.linalg.norm(x_rot_np))
+theta = np.arccos(cos_theta) * 180 / np.pi
 
 # Set the limits of the plot
 ax.set_xlim([-3, 3])
-ax.set_ylim([-1, 4])
+ax.set_ylim([-3, 3])
 
 # Add labels and title
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
-ax.set_title('Vector Plot')
+ax.set_title('GA Vector Plot')
+
+# Add text annotation for the angle between the vectors
+ax.text(0.5, 2.5, f'Angle: {theta:.2f} degrees', fontsize=12)
 
 # Show the plot
 plt.show()
